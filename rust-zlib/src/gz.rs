99001d3a637cf2a8d7140302130c4efef788f414
Francis Nimick <fnimick@gmail.com>
2015-04-02 20:26:25 -0400

    support crc

--- rust-zlib/src/gz.rs
+++ rust-zlib/src/gz.rs
@@ -2,18 +2,40 @@ use cvec::{CVec, Buf};
 use libc::{c_int, c_uint, c_ulong, c_char, c_uchar, c_void, size_t};
 use std::ptr;
 
-fn get_uncompressed_len(buffer: Buf) -> usize {
+
+// every gzip file is at least 10 bytes, if not, it's invalid
+const GZIP_MIN_LEN: usize = 40;
+
+const GZIP_FILESIZE_OFFSET: usize = 4;
+const GZIP_CRC_OFFSET: usize = 8;
+
+fn get_uncompressed_len(buffer: &Buf) -> usize {
+    assert!(buffer.len() > GZIP_MIN_LEN);
     unsafe {
-        let byte_ptr = buffer.get_raw_pointer_to_item(buffer.len() - 4);
+        let byte_ptr = buffer.get_raw_pointer_to_item(buffer.len() - GZIP_FILESIZE_OFFSET);
+        ptr::read(byte_ptr as *const c_uint) as usize
+    }
+}
+
+fn get_crc(buffer: &Buf) -> usize {
+    assert!(buffer.len() > GZIP_MIN_LEN);
+    unsafe {
+        let byte_ptr = buffer.get_raw_pointer_to_item(buffer.len() - GZIP_CRC_OFFSET);
         ptr::read(byte_ptr as *const c_uint) as usize
     }
 }
 
 
 
+
 pub fn decompress(buffer: Buf) -> Option<Buf> {
-    let out_len = get_uncompressed_len(buffer);
-    println!("Out_len: {}", out_len);
+    if buffer.len() < GZIP_MIN_LEN {
+        return None;
+    }
+    let out_len = get_uncompressed_len(&buffer);
+    let crc = get_crc(&buffer);
+    println!("out_len: {}", out_len);
+    println!("crc: {}", crc);
     let out_buf = try_opt!(CVec::with_capacity(out_len));
     Some(out_buf)
 }
